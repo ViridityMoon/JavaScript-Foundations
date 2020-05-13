0b44604169b4e0a0fa24e1f50bcd7e852ecf36a2
ViridityMoon <viriditymoon@gmail.com>
2020-05-13 16:38:52 -0400

    Did the fist two questions

--- index.js
+++ index.js
@@ -3,9 +3,10 @@
 // 🏡 Task 1: Variables
 /* Create variables for principal, interest rate, and years. Assign them the values 200000, 0.05, and 30 respectively. Create another value called name and give it the value of your own name.
 */
-
-
-
+const principal = 200000;
+const interestRate = 0.05;
+const years = 30;
+const name = 'Noah';
 
 
 // 🏡 Task 1.5: Simple Math
@@ -16,6 +17,8 @@ Create a variable called `monthlyInterestRate` and give it the value of interest
 Create another variable called `periods` and give it the value of years*12.
 */
 
+const monthlyInterestRate = (interestRate/12);
+const periods = (years*12);
 
 
 
@@ -28,7 +31,9 @@ Hint #2: you'll need to use the `math` object for parts of this calculation!
 
 When your math is correct, monthlyRate will equal 1073.64
 */
+const monthlyRate = principal*[monthlyInterestRate*Math.pow((1+monthlyInterestRate),periods)]/[Math.pow((1+monthlyInterestRate),periods-1)];
 
+console.log(monthlyRate);
 
 
 
